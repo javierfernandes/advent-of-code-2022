1997a6700633841455efdc5346bb3f99cde9f9d6
Javier Fernandes <javier.fernandes@gmail.com>
2022-12-02 08:04:42 -0300

    first polish. Using map of scores for shapes. Adding ramda to compose functions

--- src/02/index.ts
+++ src/02/index.ts
@@ -1,18 +1,31 @@
+import { pipe }  from 'ramda'
 //
 // --- Day 2: Rock Paper Scissors ---
 //
 // What would your total score be if everything goes exactly according to your strategy guide?
 //
-const WIN_POINTS = 6
-const TIE_POINTS = 3
-const LOSE_POINTS = 0
 
 const THEM_ROCK = 'A', THEM_PAPER = 'B', THEM_SCISSOR = 'C'
 const ME_ROCK = 'X', ME_PAPER = 'Y', ME_SCISSOR = 'Z'
 
-const parse = (s: string) : [string, string] => s.split(' ') as [string, string]
+const WIN_POINTS = 6
+const TIE_POINTS = 3
+const LOSE_POINTS = 0
 
-const computePlayScore = ([them, me]: [string, string]): number => {
+const SHAPE_POINTS : { [key: string]: number } = {
+    [ME_ROCK]: 1,
+    [ME_PAPER]: 2,
+    [ME_SCISSOR]: 3
+}
+
+type PlayInstruction = [string, string]
+
+//
+//
+
+const parse = (s: string) : PlayInstruction => s.split(' ') as PlayInstruction
+
+const computePlayScore = ([them, me]: PlayInstruction): number => {
     if (them === THEM_ROCK) {
         if (me === ME_ROCK) return TIE_POINTS
         if (me === ME_PAPER) return WIN_POINTS
@@ -31,20 +44,11 @@ const computePlayScore = ([them, me]: [string, string]): number => {
     throw new Error(`Unknown combination ${them} - ${me}`)
 }
 
-const shapeScore = (shape: string): number => {
-    switch(shape) {
-        case ME_ROCK: return 1
-        case ME_PAPER: return 2
-        case ME_SCISSOR: return 3
-        default: throw new Error(`Unknown shape ${shape}`)
-    }
-}
-const computeRoundScore = (play: [string, string]) => computePlayScore(play) + shapeScore(play[1])
-const playAndComputeScore = (play: string): number => computeRoundScore(parse(play))
 
-export const exercise = (plays: string[]): number =>
-    plays.reduce((total: number, play) => {
-        const score  = playAndComputeScore(play)
-        console.log(`ROUND = ${play}, SCORE = ${score}, TOTAL = ${total + score}`)
-        return total + score
-    }, 0)
+const shapeScore = (shape: string) => SHAPE_POINTS[shape]
+
+const computeRoundScore = (play: PlayInstruction) => computePlayScore(play) + shapeScore(play[1])
+const playAndComputeScore = pipe(parse, computeRoundScore)
+
+export const exercise = (plays: string[]) =>
+    plays.reduce((total, play) => total + playAndComputeScore(play), 0)
